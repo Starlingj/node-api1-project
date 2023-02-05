175853d085619f25ee5cba58f4254d0df293887c
james starling <e.s.gbeast@gmail.com>
2023-02-05 18:02:40 -0500

    get all users and by id working

--- api/server.js
+++ api/server.js
@@ -1,9 +1,39 @@
-// BUILD YOUR SERVER HERE
+
+
 const express = require('express')
 const server = express()
+const User = require('./users/model')
+
+server.get('/api/users', (req, res)=>{
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
+
+server.get('/api/users/:id', (req, res)=>{
+    User.findById(req.params.id)
+    .then(user => {
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting user',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
 
 server.use('*', (req, res)=> {
     res.status(404).json({
         message:'not found'})
 })
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+module.exports = server;
